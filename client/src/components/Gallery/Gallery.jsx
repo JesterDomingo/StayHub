1e9851a198e82b3b34a85f018ae386934cabb7d8
AndreyYermakov <yermakovandrey92@gmail.com>
2024-03-26 14:47:46 -0400

    COMP231-1  adds modal with slider

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,22 +1,44 @@
 import React from "react";
+import { Modal } from "@material-ui/core";
+import SliderGallery from "../SliderGallery/SliderGallery";
+
+import { useState } from "react";
 
 import "./gallery.scss"
 
 export default function Gallery() {
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const handleModal = () => setIsOpen(!isOpen);
+
     return (
         <section className="gallery">
-            <figure className="gallery__item gallery__item--1">
+            <button onClick={handleModal} type="button" className="gallery__item gallery__item--1">
                 <img src="../assets/images/test-img.webp" className="gallery__img" alt="Image 1"></img>
-            </figure>
-            <figure className="gallery__item gallery__item--2">
+            </button>
+            <button onClick={handleModal} type="button" className="gallery__item gallery__item--2">
                 <img src="../assets/images/test-img.webp" alt="Gallery image 2" className="gallery__img"></img>
-            </figure>
-            <figure className="gallery__item gallery__item--3">
+            </button>
+            <button onClick={handleModal} type="button" className="gallery__item gallery__item--3">
                 <img src="../assets/images/test-img.webp" alt="Gallery image 3" className="gallery__img"></img>
-            </figure>
-            <figure className="gallery__item gallery__item--4">
+            </button>
+            <button onClick={handleModal} type="button" className="gallery__item gallery__item--4">
                 <img src="../assets/images/test-img.webp" alt="Gallery image 4" className="gallery__img"></img>
-            </figure>
+            </button>
+
+            <Modal
+                open={isOpen}
+                onClose={handleModal}
+            >
+                <div className="gallery__modal-wrap">
+                    <SliderGallery></SliderGallery>
+                    <button onClick={handleModal} type="button" className="gallery__modal-close">
+                        <span class="material-symbols-outlined">
+                            close
+                        </span>
+                    </button>
+                </div>
+            </Modal>
         </section>
     )
 }
--- client/src/components/SliderGallery/SliderGallery.jsx
+++ client/src/components/SliderGallery/SliderGallery.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Slider from "react-slick";
 
+import "./sliderGallery.css"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function SliderGallery (props) {
     const settings = {
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -19,7 +20,7 @@ export default function SliderGallery (props) {
           {array.map(item => {
             return (
             <div>
-                <img style={{margin: '0 auto'}} src="/assets/images/test-img.webp"></img>
+                <img key={item} style={{margin: '0 auto'}} src="/assets/images/test-img.webp"></img>
             </div>
             )
           })}
